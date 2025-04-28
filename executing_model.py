8c3faa025986cf5d8b9fb122e582da33f8be296f
baduyne <baduynguyen3172001@gmail.com>
2025-04-28 12:59:31 +0700

    update augmentation

--- executing_model.py
+++ executing_model.py
@@ -81,7 +81,7 @@ def predict_age_gender(frame):
     transform = transforms.Compose([ 
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize([0.5], [0.5])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     input_tensor = transform(pil_img).unsqueeze(0)
     
